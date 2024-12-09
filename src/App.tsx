4ec1b773f427516ab6de52b50f01201d385a0825
hirocy <hirocy@pixiv.co.jp>
2024-12-10 02:12:16 +0900

    feat: introduce tailwind and impl some features

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button type={"button"} onClick={() => setCount((count) => count + 1)}>
+		<div
+			className={
+				"min-h-screen transition-colors duration-300 bg-white dark:bg-zinc-800 m-0 flex flex-col justify-center items-center min-w-64 gap-2"
+			}
+		>
+			<p className="text-white text-5xl font-sans">hirocy / Hiroki Sugiyama</p>
+			<p className="text-white text-xl font-sans">Vite + React</p>
+			<div className="flex gap-2 justify-center items-center">
+				<button
+					type={"button"}
+					className="font-sans rounded bg-sky-600 py-2 px-4 text-sm text-white hover:bg-sky-500 active:bg-sky-700"
+					onClick={() => setCount((count) => count + 1)}
+				>
 					count is {count}
 				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
+				<button
+					type={"button"}
+					disabled
+					className="font-sans rounded bg-sky-600 py-2 px-4 text-sm text-white hover:bg-sky-500 active:bg-sky-700"
+				>
+					disabled
+				</button>
 			</div>
-			<p className="read-the-docs">
+			<p className="text-white text-sm font-sans">
 				Click on the Vite and React logos to learn more
 			</p>
-		</>
+		</div>
 	);
 }
 
